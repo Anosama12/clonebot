b610dfeed98932505906858ccc22a42ad4cb92d2
Anosama12 <samepatel002@gmail.com>
2024-08-09 15:49:58 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -11,16 +11,16 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 class Config(object):
 
     # Get a bot token from botfather
-    TG_BOT_TOKEN = "Your_Telegram_bot_token"
+    TG_BOT_TOKEN = "6753060421:AAFzWtkn6xLmOY5EM_OKbae80MQJGWgVkbU"
 
     # Get from my.telegram.org
-    APP_ID = int(Your_telegram_App_Id)
+    APP_ID = int(24367625)
 
     # Get from my.telegram.org
-    API_HASH = "Your_Telegram_Api_Hash"
+    API_HASH = "b27d49e801eca418acc68d35c5fa768e"
 
     # Generate a user session string
-    TG_USER_SESSION = "Your_user_session_string_compatible_with_Pyrogram_v2"
+    TG_USER_SESSION = "BQG2SaYALSFUemAdNPhFRG3jekt3Zk3T-_WB6jdR9GR0035e8GRcQKshQYgj1TQIFWInCrxTm_ASl30ftxu9wqbotis7DRzzr6XxC_xJ-YdjpnE6ktWudB0ss6XNjSwR92iUI-9B0ewCvaVNws54PFySK4dK41BIGnPwgXJI2ifNt-sVnf8Qe4FQQKfB7RGcvqAowGy2vzoc1JnqBTU9kHk0dk_TTiJeCKlMrMbFD6IiYQWVhO1VxCFEUL9C9_hC-q2DjWtPO7U7ne_ntCKUtlT88sFGR_8DYFdYF172C-rToepj-yeyeSGc623OPRVdACQiTCFwsuPZ9VX_ZT6VgK0PQpM4PAAAAABXZYVDAA"
 
     # Database URI
     DB_URI = "Your_database_URI"
